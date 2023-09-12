947a850329c6b14456b532dd0cb20a91042a642f
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2023-09-11 17:38:18 -0700

    Create a grid with the number of squares the user entered.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,21 @@
 const containerEl = document.querySelector(".container");
+const buttonEl = document.querySelector("button");
 
-for (let i=1; i<=256; i++) {
-    const divEl = document.createElement("div");
-    divEl.innerText=i;
-    containerEl.append(divEl);
+function createGrid(num) {
+    for (let i=1; i<=num*num; i++) {
+        const divEl = document.createElement("div");
+        divEl.innerText=i;
+        divEl.style.width= `calc(100%/${num})`
+        divEl.style.height= `calc(100%/${num})`
+        containerEl.append(divEl);
+        divEl.addEventListener("mouseover", () => {
+            divEl.style.backgroundColor = "green";
+        } )
+    }
 }
+
+buttonEl.addEventListener("click", ()=> {
+    let numSquares = parseInt(prompt("Insert number of squares per grid"));
+    createGrid(numSquares);     
+})
+
